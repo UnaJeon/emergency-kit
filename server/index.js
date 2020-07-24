027e5c7f86fa6ba3296be244a921e5125d3fa3f7
UnaJeon <unajeon@gmail.com>
2020-07-24 19:06:08 +0000

    added get reqeust for specific productId

--- server/index.js
+++ server/index.js
@@ -29,6 +29,26 @@ app.get('/api/products', (req, res, next) => {
       .catch(err => next(err)));
 });
 
+app.get('/api/products/:productId', (req, res, next) => {
+  const productId = Number(req.params.productId);
+  const sql = `
+  select *
+  from "products"
+  where "productId" = $1
+  `;
+  const params = [productId];
+  db.query(sql, params)
+    .then(result => {
+      const product = result.rows[0];
+      if (!product) {
+        next(new ClientError(`cannot find with "productId" with ${productId}`, 404));
+      } else {
+        res.json(product);
+      }
+    })
+    .catch(err => next(err));
+});
+
 app.use('/api', (req, res, next) => {
   next(new ClientError(`cannot ${req.method} ${req.originalUrl}`, 404));
 });
