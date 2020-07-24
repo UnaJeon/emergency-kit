d8e3e31a06355b9850f37956827f7113ce2f9a1b
UnaJeon <unajeon@gmail.com>
2020-07-24 03:55:32 +0000

    Added header and product list item components

---
+++ client/components/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <header>
+        <h3><i className = "fas fa-dollar-sign"></i>Wicked Sales</h3>
+      </header>
+    );
+
+  }
+}
