c92ba19219ba2a321cca04a56a4fda7900d53e7f
UnaJeon <unajeon@gmail.com>
2020-07-24 08:10:52 +0000

    changed price format

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
 
 export default function ProductListItem(props) {
-  return (
+  const getPrice = props.price;
+  const pricetoString = getPrice.toString();
+  const priceArray = pricetoString.split('');
+  priceArray.splice(priceArray.length - 2, 0, '.');
+  const price = '$' + priceArray.join('');
 
+  return (
     <div className="card col">
       <div className="imageContainer">
         <img src={props.image} className="card-img-top" />
       </div>
       <div className="card-body">
         <h5 className="card-title">{props.name}</h5>
-        <h6 className="card-subtitle mb-2 text-muted">{props.price}</h6>
+        <h6 className="card-subtitle mb-2 text-muted">{price}</h6>
         <p className="card-text">{props.shortDescription}</p>
       </div>
     </div>
