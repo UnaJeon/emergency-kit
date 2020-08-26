18436acbd819bb71c674e0e6f102e153433547e6
UnaJeon <unajeon@gmail.com>
2020-08-26 07:31:40 +0000

    change the icon on head element and modal

--- client/components/app.jsx
+++ client/components/app.jsx
@@ -15,7 +15,8 @@ export default class App extends React.Component {
         name: 'catalog',
         params: {}
       },
-      cart: []
+      cart: [],
+      show: true
     };
     this.setView = this.setView.bind(this);
     this.addToCart = this.addToCart.bind(this);
@@ -70,22 +71,56 @@ export default class App extends React.Component {
     this.getCartItems();
   }
 
-  render() {
-    let page = null;
-    if (this.state.view.name === 'catalog') {
-      page = <ProductList setView={this.setView} />;
-    } else if (this.state.view.name === 'details') {
-      page = <ProductDetails params={this.state.view.params} setView={this.setView} addToCart={this.addToCart} />;
-    } else if (this.state.view.name === 'cart') {
-      page = <CartSummary cart = {this.state.cart} setView = {this.setView}/>;
-    } else if (this.state.view.name === 'checkout') {
-      page = <CheckoutForm setView={this.setView} placeOrder={this.placeOrder} cart={this.state.cart}/>;
-    }
+  showModal() {
+    this.setState({ show: true });
+  }
+
+  renderModal() {
     return (
-      <div>
-        <Header setView={this.setView} cartItemCount={this.state.cart.length} />
-        {page}
+      <div className="modal" tabIndex="-1" role="dialog">
+        <div className = "modal-dialog" role="document">
+          <div className ="modal-content">
+            <div className="modal-header">
+              <h5 className ="modal-title">Be Ready for the Emergency</h5>
+              <button type="button" className="close" data-dismiss="modal" aria-label="Close"></button>
+              <span aria-hidden="true">&times;</span>
+            </div>
+            <div className="modal-body">
+              <p>Please note that this website is a content management application created for the purpose of demonstration. Check the box below
+                to acknowledge that the merchandise shown here is not available for purchase, that you will not provide genuine financial or personal information,
+                and that you are aware no purchase will truly be processed.</p>
+              <input type="checkbot">I acknowledge that this is strictly a demo application</input>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className ="btn btn-danger">Submit</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
+  }
+
+  render() {
+    if (!this.state.show) {
+      return null;
+    } else {
+      let page = null;
+      if (this.state.view.name === 'catalog') {
+        page = <ProductList setView={this.setView} />;
+      } else if (this.state.view.name === 'details') {
+        page = <ProductDetails params={this.state.view.params} setView={this.setView} addToCart={this.addToCart} />;
+      } else if (this.state.view.name === 'cart') {
+        page = <CartSummary cart={this.state.cart} setView={this.setView} />;
+      } else if (this.state.view.name === 'checkout') {
+        page = <CheckoutForm setView={this.setView} placeOrder={this.placeOrder} cart={this.state.cart} />;
+      }
+      return (
+        <div>
+          <Header setView={this.setView} cartItemCount={this.state.cart.length} />
+          {page}
+        </div>
+      );
+    }
+
   }
 }
