f1d31f44519db63294764899a090a4a281014c4b
UnaJeon <unajeon@gmail.com>
2020-09-04 06:08:40 +0000

    changed styling on cart summary

--- client/components/cart-summary-item.jsx
+++ client/components/cart-summary-item.jsx
@@ -3,15 +3,13 @@ import React from 'react';
 export default class CartSummaryItem extends React.Component {
   render() {
     return (
-      <div className="container mt-5" style={{ border: '1px solid grey' }}>
-        <div className="row justify-content-md-center m-2">
-          <div className="row d-flex justify-content-center pt-2 mt-3">
-            <img src={this.props.image} alt="product" />
-            <div className="ml-4">
-              <h3>{this.props.name}</h3>
-              <div>{this.props.price}</div>
-              <p>{this.props.shortDescription}</p>
-            </div>
+      <div className="container mt-3" style={{ border: '1px solid grey' }}>
+        <div className="d-flex flex-nowrap justify-content-around m-3">
+          <img src={this.props.image} alt="product" />
+          <div className="mt-5">
+            <h3>{this.props.name}</h3>
+            <div>{this.props.price}</div>
+            <p>{this.props.shortDescription}</p>
           </div>
         </div>
       </div>
