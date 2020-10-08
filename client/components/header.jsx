1b5ca961626cb7e576f86067d4dbee8479135e29
UnaJeon <unajeon@gmail.com>
2020-10-08 01:01:50 +0000

    user can click item count and image to go to chekcout section

--- client/components/header.jsx
+++ client/components/header.jsx
@@ -5,7 +5,7 @@ export default class Header extends React.Component {
     return (
       <header className="p-2 bg-danger text-white d-flex justify-content-between">
         <p onClick={() => this.props.setView('catalog', {})} style={{ cursor: 'pointer' }}><i className="fas fa-first-aid mt-3 mr-1 ml-5"></i>Emergency Kit</p>
-        <p className="mr-5 mt-3">{this.props.cartItemCount}<span className="ml-2 ">Items</span><i className="fa fa-shopping-cart ml-2" aria-hidden="true" style={{ cursor: 'pointer' }} onClick={() => this.props.setView('cart', {})}></i></p>
+        <p className="mr-5 mt-3" onClick={() => this.props.setView('cart', {})} style={{ cursor: 'pointer' }}>{this.props.cartItemCount}<span className="ml-2 " >Items</span><i className="fa fa-shopping-cart ml-2" aria-hidden="true"></i></p>
       </header>
     );
 
